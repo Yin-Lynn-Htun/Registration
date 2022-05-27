cc641f39cb94088e0a2af08adbef6bcef66ea0e3
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-05-27 07:15:16 +0630

    refactor Controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,34 +13,6 @@ class StudentController extends Controller
         return Student::orderby('id', 'DESC')->with('courses')->get();
     }
 
-    // public function create() {
-    //     $student = new Student;
-    //     $student->name = 'Mg Mg';
-    //     $student->dob = Carbon::now();
-    //     $student->email = 'mgmg@gmail.com';
-    //     $student->nrc = '12/asdf(n)1212';
-    //     $student->save();
-
-    //     $courses = Course::find([1,2,3]);
-    //     $student->courses()->attach($courses);
-
-    //     return $student;
-    // }
-
-    // public function store(Request $request) {
-    //     $student = new Student; 
-    //     $student->name = $request->name;
-    //     $student->email = $request->email;
-    //     $student->nrc = $request->nrc;
-    //     $student->dob = $request->dob;
-    //     $student->save();
-
-    //     $courses = Course::find($request->input('courses'));
-    //     $student->courses()->attach($courses);
-
-    //     return redirect('/');
-    // }
-
     public function store(Request $request) {
         $time = strtotime($request->student['dob']);
         $newformat = date('Y-m-d',$time);
@@ -58,13 +30,13 @@ class StudentController extends Controller
         return $student;
     }
 
-    public function destory($id) {
-        $existingItem = Student::find($id);
+    // public function destory($id) {
+    //     $existingItem = Student::find($id);
 
-        if($existingItem) {
-            $existingItem->delete();
-            return "Item successfully deleted.";
-        }
-        return "No item found.";
-    }
+    //     if($existingItem) {
+    //         $existingItem->delete();
+    //         return "Item successfully deleted.";
+    //     }
+    //     return "No item found.";
+    // }
 }
